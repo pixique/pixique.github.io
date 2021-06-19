add4c3f8834111382224074a68fa7fbc2cb21085
pixique <86171668+pixique@users.noreply.github.com>
2021-06-19 19:35:19 -0400

    Update php to json

--- assets/php/contact_me.php
+++ assets/php/contact_me.php
@@ -1,21 +1,19 @@
 <?php
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $_POST['message'];
-$subject = $_POST['subject'];
-	
-// create email body and send it	
-$to = 'pixiquebrand@gmail.com'; //Just write your email, no more :)
-$email_subject = "Contact form submitted by:  $name";
-$email_body = "You have received a new message. <br/>".
-			  "Here are the details: <br/><br/> Name: $name <br/><br/> Subject: $subject<br/><br/> ".
-		      "Email: $email_address <br/><br/> Message: <br/> $message";
-$headers="From:<$email_address>\n";
-$headers.="Content-Type:text/html; charset=UTF-8";
-if($email_address != "") {
-	mail($to,$email_subject,$email_body,$headers);
-	return true;
-}
+
+header('content-type: application/json');
+
+    $o = new stdClass();
+    $o->status = 'success';
+    echo json_encode($o);
+
+    $email_to = "pixiquebrand@gmail.com"; //Just write your email, no more.
+    $email = $_POST["email"];
+    $text = "Congratulations ! A new person wants to be alerted when your site will be online: $email";
+
+    $headers = "MIME-Version: 1.0" . "\r\n"; 
+    $headers .= "Content-type:text/html; charset=utf-8" . "\r\n"; 
+    $headers .= "From:<$email>\n";
+
+    mail($email_to, "Message", $text, $headers);
 
 ?>
